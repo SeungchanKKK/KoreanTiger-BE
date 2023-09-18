6036b6a710864f301a456b0ae1f29babaf1ccee2
0AndWild <parkky3563@gmail.com>
2023-09-19 00:04:06 +0900

    korean-tiger BE project init

---
+++ src/main/java/com/geupjo/koreantiger/KoreanTigerApplication.java
@@ -0,0 +1,13 @@
+package com.geupjo.koreantiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanTigerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreanTigerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geupjo/koreantiger/KoreanTigerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geupjo.koreantiger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanTigerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
