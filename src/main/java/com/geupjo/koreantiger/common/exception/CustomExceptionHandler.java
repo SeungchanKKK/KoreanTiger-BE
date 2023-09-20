1578715bc13ea4689fde5f283792d7695bbef7f2
seungchanKKK <eccsck@gmail.com>
2023-09-21 00:44:11 +0900

    [feat] Create UserFind Error Handler

--- src/main/java/com/geupjo/koreantiger/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/geupjo/koreantiger/common/exception/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.geupjo.koreantiger.common.exception;
 
 import com.geupjo.koreantiger.common.ApiResponse;
+import com.geupjo.koreantiger.dto.response.ResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ApiResponse> handleCustomExecution(CustomException e) {
+        return ResponseEntity.badRequest().body(ApiResponse.createError(e.getErrorCode()));
+    }
+
+    //존재하지 않는 유저
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse> handleNotExistUserException(CustomException e){
         return ResponseEntity.badRequest().body(ApiResponse.createError(e.getErrorCode()));
     }
 }
--- src/main/java/com/geupjo/koreantiger/common/exception/ErrorCode.java
+++ src/main/java/com/geupjo/koreantiger/common/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     LOGIN_INVALID("로그인 정보가 잘못되었습니다.", 100),
-    PASSWORD_INVALID("비밀번호가 잘못되었습니다.", 101);
+    PASSWORD_INVALID("비밀번호가 잘못되었습니다.", 101),
+    //유저에러
+    NOTMATCH_USER_EXCEPTION("존재하지않는 유저입니다.",400);
 
     private final String message;
     private final int code;
