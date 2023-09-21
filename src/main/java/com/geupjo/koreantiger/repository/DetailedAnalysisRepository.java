5471181d695c6090ec9516c8b22de5ef2850c87d
gunyoung <parkky3563@gmail.com>
2023-09-22 08:53:21 +0900

    fix : fix build issue

--- src/main/java/com/geupjo/koreantiger/repository/DetailedAnalysisRepository.java
+++ src/main/java/com/geupjo/koreantiger/repository/DetailedAnalysisRepository.java
@@ -1,8 +1,9 @@
 package com.geupjo.koreantiger.repository;
 
+import com.geupjo.koreantiger.entity.DetailedAnalysis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetailedAnalysisRepository extends JpaRepository<DetailedAnalysisRepository, Long> {
+public interface DetailedAnalysisRepository extends JpaRepository<DetailedAnalysis, Long> {
 }
