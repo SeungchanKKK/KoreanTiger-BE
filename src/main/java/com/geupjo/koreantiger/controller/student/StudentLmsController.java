70a30769e7cd97b9606465cd8e3d904a34b0df3e
seungchanKKK <eccsck@gmail.com>
2023-09-22 10:35:03 +0900

    [fix] change controller api path

--- src/main/java/com/geupjo/koreantiger/controller/student/StudentLmsController.java
+++ src/main/java/com/geupjo/koreantiger/controller/student/StudentLmsController.java
@@ -22,13 +22,13 @@ public class StudentLmsController {
     private final StudentLmsFacade studentLmsFacade;
 
     @Operation(summary = "학생프로필 api 입니다")
-    @GetMapping("/experience")
+    @GetMapping("/studentLms/experience")
     public ApiResponse<StudentProfileDto> studentProfileResponse(@Parameter(description = "학생Id") @RequestParam(value = "studentID", required = false) Long studentId) {
         return studentLmsFacade.getStudentProfile(1L);
     }
 
     @Operation(summary = "랭킹보드 api 입니다")
-    @GetMapping("/ranking")
+    @GetMapping("/studentLms/ranking")
     public ApiResponse<RankingBoardDto> studentRankingResponse(@Parameter(description = "학생Id") @RequestParam(value = "studentID", required = false) Long studentId) {
         return studentLmsFacade.getRankingBoard(studentId);
     }
