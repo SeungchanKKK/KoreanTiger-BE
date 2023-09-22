470216dfa8492ae27f8c9d1ac2b891227b6f9de9
gunyoung <parkky3563@gmail.com>
2023-09-22 13:17:05 +0900

    refactor : refactor studentLms APIs endpoint

--- src/main/java/com/geupjo/koreantiger/controller/student/StudentLmsController.java
+++ src/main/java/com/geupjo/koreantiger/controller/student/StudentLmsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "StudentLmsController", description = "학생 LMS 창에 들어가는 기능 api입니다")
-@RequestMapping("/v1")
+@RequestMapping("/v1/studentLms")
 @RestController
 @RequiredArgsConstructor
 public class StudentLmsController {
@@ -23,19 +23,19 @@ public class StudentLmsController {
     private final StudentLmsFacade studentLmsFacade;
 
     @Operation(summary = "학생프로필 api 입니다")
-    @GetMapping("/studentLms/experience")
+    @GetMapping("/experience")
     public ApiResponse<StudentProfileDto> studentProfileResponse(@Parameter(description = "학생Id") @RequestParam(value = "studentID", required = false) Long studentId) {
         return studentLmsFacade.getStudentProfile(1L);
     }
 
     @Operation(summary = "랭킹보드 api 입니다")
-    @GetMapping("/studentLms/ranking")
+    @GetMapping("/ranking")
     public ApiResponse<RankingBoardDto> studentRankingResponse(@Parameter(description = "학생Id") @RequestParam(value = "studentID", required = false) Long studentId) {
         return studentLmsFacade.getRankingBoard(studentId);
     }
 
     @Operation(summary = "학생의 지난 1년간 학습 이력")
-    @GetMapping("/education/histories")
+    @GetMapping("/histories")
     public ApiResponse<StudentHistoryResponseDto> studentEducationHistories() {
         StudentHistoryResponseDto response = studentLmsFacade.getStudentEducationHistories();
         return ApiResponse.success(response);
