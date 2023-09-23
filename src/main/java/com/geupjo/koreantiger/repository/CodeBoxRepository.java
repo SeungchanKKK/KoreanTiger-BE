4523f6e7a6b00c436ddba956d0e7403aafe3a0bc
gunyoung <parkky3563@gmail.com>
2023-09-24 04:12:30 +0900

    feat : add learning box API

--- src/main/java/com/geupjo/koreantiger/repository/CodeBoxRepository.java
+++ src/main/java/com/geupjo/koreantiger/repository/CodeBoxRepository.java
@@ -4,6 +4,9 @@ import com.geupjo.koreantiger.entity.CodeBox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CodeBoxRepository extends JpaRepository<CodeBox, Long> {
+    List<CodeBox> findAllByMemberId(Long id);
 }
